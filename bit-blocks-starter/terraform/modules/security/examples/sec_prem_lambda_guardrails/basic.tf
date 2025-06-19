d6d6d0f87bd28c5307abe28bb2f6b58ef26a074f
or4dx <19908496+or4dx@users.noreply.github.com>
2025-06-19 16:55:19 +0400

    Removed the premium modules from the starter pack kit

--- bit-blocks-starter/terraform/modules/security/examples/sec_prem_lambda_guardrails/basic.tf
+++
@@ -1,6 +0,0 @@
-module "lambda_guardrails_basic" {
-  source         = "../../sec_prem_lambda_guardrails"  # Adjust based on your folder structure
-  region         = "us-east-1"
-  sns_topic_arn  = "arn:aws:sns:us-east-1:123456789012:security-alerts"
-  lambda_zip_path = "${path.module}/lambda_permission_checker.zip"
-}
